494fb3e272856ad5775ddaa803d59f4eba9fd97f
Siddhant Jaiswal <61560828+SiddhantJaiswal-0125@users.noreply.github.com>
2020-10-15 18:29:42 +0530

    revString

--- Special450/src/RevStringOrArray.java
+++ Special450/src/RevStringOrArray.java
@@ -1,3 +1,40 @@
+import java.util.Scanner;
+
+/*
+* link : https://www.geeksforgeeks.org/write-a-program-to-reverse-an-array-or-string/
+ */
 public class RevStringOrArray
 {
+
+    public static void main(String[] args)
+    {
+        Scanner sc  = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0)
+        {
+            String s = sc.next();
+            int start = 0;
+            int end = s.length()-1;
+            while(start< end)
+            {
+                char c = s.charAt(start);
+                char d = s.charAt(end);
+                s = s.substring(0, start)
+                        + d
+                        + s.substring(start + 1);
+                s = s.substring(0, end)
+                        + c
+                        + s.substring(end + 1);
+
+
+
+                start++;
+                end --;
+            }
+
+            System.out.println(s);
+
+
+        }
+    }
 }
