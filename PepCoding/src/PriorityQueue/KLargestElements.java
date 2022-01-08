4d24c045fea0e25583107de22cc093dfdd3d0d5e
SiddhantJaiswal-0125 <siddhantjaiswal363@gmail.com>
2022-01-09 00:26:12 +0530

    Heap - PriorityQueue

---
+++ PepCoding/src/PriorityQueue/KLargestElements.java
@@ -0,0 +1,37 @@
+package PriorityQueue;
+import java.io.*;
+import java.util.*;
+
+
+public class KLargestElements {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int k = Integer.parseInt(br.readLine());
+
+        PriorityQueue <Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i:arr)
+            pq.add(i);
+        int i = 0;
+        int ans []= new int[k];
+        while(i<k)
+        {
+            ans[i] = pq.remove();
+            i++;
+        }
+
+        for(int in = k-1;in>=0;in--)
+            System.out.println(ans[in]);
+
+
+
+    }
+
+}
