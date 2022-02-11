8a237e36e9fcde96b14013e347dd3f0d8d8711a5
SiddhantJaiswal-0125 <siddhantjaiswal363@gmail.com>
2022-02-11 13:48:04 +0530

    Level 2 Started

---
+++ PepCoding/src/Level2/Btree/MorisTraversal.java
@@ -0,0 +1,68 @@
+package Level2.Btree;
+import java.util.*;
+
+public class MorisTraversal {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static ArrayList<Integer> morrisInTraversal(TreeNode Treenode) {
+
+        ArrayList<Integer> ar = new ArrayList<>();
+        inorder(Treenode,ar);
+        return ar;
+
+
+    }
+    static void inorder(TreeNode node,  ArrayList<Integer> ar)
+    {
+        if(node  ==  null)
+            return ;
+        inorder(node.left,ar);
+        ar.add(node.val);
+        inorder(node.right,ar);
+        return ;
+
+    }
+
+    // input_section=================================================
+
+    public static TreeNode createTree(int[] arr, int[] IDX) {
+        if (IDX[0] > arr.length || arr[IDX[0]] == -1) {
+            IDX[0]++;
+            return null;
+        }
+        TreeNode Treenode = new TreeNode(arr[IDX[0]++]);
+        Treenode.left = createTree(arr, IDX);
+        Treenode.right = createTree(arr, IDX);
+
+        return Treenode;
+    }
+
+    public static void solve() {
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = scn.nextInt();
+
+        int[] IDX = new int[1];
+        TreeNode root = createTree(arr, IDX);
+
+        ArrayList<Integer> ans = morrisInTraversal(root);
+        for (Integer i : ans)
+            System.out.print(i + " ");
+
+    }
+
+    public static void main(String[] args) {
+        solve();
+    }
+}
