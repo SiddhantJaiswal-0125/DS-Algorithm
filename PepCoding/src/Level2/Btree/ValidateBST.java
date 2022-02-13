8c61ba58b5c67b9c9eda99870c60e14aaae9d967
SiddhantJaiswal-0125 <siddhantjaiswal363@gmail.com>
2022-02-13 22:17:41 +0530

    Validate BST

--- PepCoding/src/Level2/Btree/ValidateBST.java
+++ PepCoding/src/Level2/Btree/ValidateBST.java
@@ -1,2 +1,61 @@
-package Level2.Btree;public class ValidateBST {
+package Level2.Btree;
+import java.util.Scanner;
+
+public class ValidateBST {
+    public static Scanner scn = new Scanner(System.in);
+
+    public static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static boolean isValidBST(TreeNode root) {
+
+        return check(Integer.MIN_VALUE, Integer.MAX_VALUE, root);
+
+    }
+    static boolean check(int min, int max, TreeNode root)
+    {
+        if(root ==  null)
+            return true;
+
+
+        if(root.val >= max || root.val <= min)
+            return false;
+
+        return check(min , root.val  , root.left) && check(root.val, max, root.right);
+    }
+
+    public static TreeNode createTree(int[] arr, int[] IDX) {
+        if (IDX[0] > arr.length || arr[IDX[0]] == -1){
+            IDX[0]++;
+            return null;
+        }
+
+        TreeNode node = new TreeNode(arr[IDX[0]++]);
+        node.left = createTree(arr, IDX);
+        node.right = createTree(arr, IDX);
+
+        return node;
+    }
+
+    public static void solve() {
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = scn.nextInt();
+
+        int[] IDX = new int[1];
+        TreeNode root = createTree(arr, IDX);
+        System.out.println(isValidBST(root));
+    }
+
+    public static void main(String[] args) {
+        solve();
+    }
 }
