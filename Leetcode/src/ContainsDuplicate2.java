8986aeb915829810a28b1eba355b78c25afdf5b3
SiddhantJaiswal-0125 <siddhantjaiswal363@gmail.com>
2022-02-23 21:23:44 +0530

    Contains Duplicate 2

---
+++ Leetcode/src/ContainsDuplicate2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class ContainsDuplicate2
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ar[] = new int[n];
+        for(int i =0;i<n;i++)
+            ar[i] = sc.nextInt();
+        int k = sc.nextInt();
+        boolean ans = new ContainsDuplicate2().containsNearbyDuplicate(ar, k);
+        System.out.println(ans);
+    }
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(i > k) set.remove(nums[i-k-1]);
+            if(!set.add(nums[i])) return true;
+        }
+        return false;
+    }
+}
