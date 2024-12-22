e68cda0bc7bc107e115cc0973d700d5edfaa7f3e
Siddhant Jaiswal <siddhantjaiswal363@gmail.com>
2024-12-22 18:33:31 +0530

    Sudoko Solver

--- SDE120/src/Recursion/SudukoSolver.java
+++ SDE120/src/Recursion/SudukoSolver.java
@@ -5,67 +5,84 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class SudukoSolver {
-
+//    public static void main(String[] args)
+//    {
+//
+//        int row = 4;
+//        for(int i =1 ; i<9;i++)
+//        {
+//            int v = 3 * (row/3) + i /3;
+//            System.out.println(v);
+//        }
+//
+//    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
-        int  mat[][] = new int[m][m];
-        for(int i =0;i<m;i++)
-            for(int j =0;j<m;j++)
-                mat[i][j] = sc.nextInt();
-            ArrayList<String> ans = findPath(mat,m);
-            if(ans.size() >0)
-            {
-                for(int i = 0;i<ans.size();i++)
-                    System.out.print(ans.get(i)+"\t");
-                System.out.println();
+        char  board[][] = new char[m][m];
+        for(int i =0;i<m;i++) {
 
-            }
-            else
-                System.out.println(-1);
-    }
-    public static ArrayList<String> findPath(int[][] m, int n) {
-        // Your code here
-        ArrayList<String> res = new ArrayList<>();
-        boolean isVis[][]= new boolean[n][n];
-        //FOR DOWN
-        solve(m,n,0,0,res,"" ,isVis);
-
-
-        Collections.sort(res);
-        return res;
-    }
-    static void solve(int [][]m, int n, int r, int c,
-                      ArrayList<String>res,String asf, boolean isVis[][])
-    {
-        if(r>=n || c>=n || r<0||c<0)
-            return;
-        if(m[r][c] == 0)
-            return;
-
-        if(r==n-1 && c==n-1)
-        {
-            res.add(new String(asf));
-            return;
+            String ip = sc.next();
+            for (int j = 0; j < m; j++)
+                board[i][j] = ip.charAt(j);
         }
-        if(isVis[r][c])
-            return;
-        isVis[r][c] = true;
-        //Down
-        solve(m,n,r+1, c, res, asf+"D",isVis);
-        //Left
-        solve(m,n,r, c-1, res, asf+"L",isVis);
-        //Rigth
-        solve(m,n,r, c+1, res, asf+"R",isVis);
-        //Up
-        solve(m,n,r-1, c, res, asf+"U",isVis);
-        isVis[r][c] = false;
 
-
-
-        return;
-
+       for(int i = 0; i<m;i++)
+       {
+           for(int j = 0; j<m;j++)
+               System.out.print(board[i][j] +"\t");
+           System.out.println();
+       }
 
     }
+
+    public void solveSudoku(char[][] board) {
+        solveKro(board);
+        return;
+    }
+
+    boolean solveKro(char [][]  board)
+    {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (checkKarkeBataoSahiHaiYaNahi(board, i, j, c)) {
+                            board[i][j] = c;
+
+                            if (solveKro(board))
+                                return true;
+                            else
+                                board[i][j] = '.';
+                        }
+                    }
+
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    boolean checkKarkeBataoSahiHaiYaNahi(char [][]  board, int i, int j, char ch)
+    {
+        for(int idx=  0; idx<9;idx++)
+        {
+            if(board[i][idx] == ch)return false;
+            if(board[idx][j] == ch) return false;
+
+
+            //THIS IS THE MAIN LOGIC :: TODO:
+
+            if(board[3 * (i/3) + idx/3] [3*(j/3)+idx%3]  == ch    )return  false;
+
+        }
+
+        return  true;
+    }
+
+
+
 }
 
